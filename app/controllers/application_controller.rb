4aa878e1c4a628fa75ed0f9b43df0756a931fccc
Simon Wathigo <wathigosimon@gmail.com>
2020-05-13 09:32:00 +0300

    Fix: Fixed bugs the application controller and user specs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,15 @@
+# frozen_string_literal: true
+
 class ApplicationController < ActionController::API
-    include Response
-    include ExceptionHandler
+  include Response
+  include ExceptionHandler
 
-    before_action :authorize_request
-    attr_reader :current_user
+  before_action :authorize_request
+  attr_reader :current_user
 
-    private
+  private
 
-    def authoriza_request
-        @current_user = (AuthorizeApiRequest.new(request.headers).call)[:user]
-    end
+  def authorize_request
+    @current_user = AuthorizeApiRequest.new(request.headers).call[:user]
+  end
 end
