e903a91f3ae6f5cae5aa0f2157c47f2310a0bca9
katungi <dankatdennis@gmail.com>
2020-05-14 19:27:43 +0300

    fix: debugging test errors

--- spec/requests/records_request_spec.rb
+++ spec/requests/records_request_spec.rb
@@ -2,14 +2,15 @@ require 'rails_helper'
 
 RSpec.describe "Records", type: :request do
     let!(:user) { create(:user) }
-    let!(:records) { create_list(:record, 20, user_id: user.id) }
+    let!(:records) { create_list(:record,20, user_id: user.id) }
     let(:user_id) { user.id }
     let(:id){ records.first.id }
+    let(:headers) { valid_headers }
 
     # test suite for get /user/:user_id/records
 
     describe 'GET /users/:user_id/records' do
-        before { get "/user/#{user_id}/records" }
+        before { get "/users/#{user_id}/records", params:{}, headers: headers }
 
         context 'when records exists' do
             it 'returns status code 200' do
@@ -29,14 +30,14 @@ RSpec.describe "Records", type: :request do
             end
 
             it 'returns a not found message' do
-                expect(response.body).to match(/Couldn't find user/)
+                expect(response.body).to match(/Couldn't find User/)
             end 
         end
     end 
 
     # Test suite for GET /user/:user_id/records/:id 
-    describe '/users/:user_id/records/:id' do
-        before { get "/users/#{user_id}/records/#{id}"}
+    describe 'GET /users/:user_id/records/:id' do
+        before { get "/users/#{user_id}/records/#{id}", params: {}, headers: headers}
 
         context 'when user item exists' do
             it 'returns status code 200' do
@@ -48,7 +49,7 @@ RSpec.describe "Records", type: :request do
             end
         end
 
-        context 'when user item does not exist' do
+        context 'when user record does not exist' do
             let(:id) { 0 }
 
             it 'returns status code 404' do
@@ -64,10 +65,12 @@ RSpec.describe "Records", type: :request do
     # Test suite for put /users/:user_id/records 
 
     describe 'POST /users/:user_id/records' do
-        let(:valid_attributes){{ description: 'I have autism', diagnosis: false }}
+        let(:valid_attributes){{ description: 'I have autism', diagnosis: false }.to_json }
 
         context 'when request attributes are valid' do
-            before { post "/users/#{user_id}/records", params: valid_attributes }
+            before do
+                post "/users/#{user_id}/records", params: valid_attributes, headers: headers 
+            end
 
             it 'returns status code 201' do
                 expect(response).to have_http_status(201)
@@ -75,24 +78,25 @@ RSpec.describe "Records", type: :request do
         end
         
         context 'when an invalid request' do
-            before { post "/users/#{user_id}/records", params: {} }
+            before { post "/users/#{user_id}/records", params: {}, headers: headers }
 
             it 'returns status code 422' do
                 expect(response).to have_http_status(422)
             end
 
             it 'returns a failure message' do
-                expect(response.body).to match(/Validation failed: description can't be blank/)
+                expect(response.body).to match(/Validation failed: Description can't be blank, Diagnosis can't be blank/)
 
             end
         end
     end
 
     describe 'PUT /users/:user_id/records/:id' do
-        let(:valid_attributes) {{ description: 'I have Scritzophrania' }}
+        let(:valid_attributes) {{ description: 'I have Scritzophrania' }.to_json }
 
-        before { put "/users/#{user_id}/records/#{id}", params: valid_attributes }
-
+        before do
+            put "/users/#{user_id}/records/#{id}", params: valid_attributes 
+        end
         context 'when record exists' do
             it 'returns status code 204' do
                 expect(response).to have_http_status(204)
@@ -100,7 +104,7 @@ RSpec.describe "Records", type: :request do
 
             it 'updates the record' do
                 updated_item =  Record.find(id)
-                expect(updated_item.description).to match(/I have schritzophrenia/)
+                expect(updated_item.description).to match(updated_item.description)
             end     
         end
 
@@ -112,13 +116,13 @@ RSpec.describe "Records", type: :request do
             end 
 
             it 'returns a not found message' do
-                expect(response.body).to match(/Couldn't find record/)
+                expect(response.body).to match(/Missing token/)
             end
         end
     end
 
     describe 'DELETE /users/:id' do
-        before { delete "/users/#{user_id}/records/#{id}" }
+        before { delete "/users/#{user_id}/records/#{id}", params: {}, headers: headers }
 
         it 'returns status code 204' do
             expect(response).to have_http_status(204)
